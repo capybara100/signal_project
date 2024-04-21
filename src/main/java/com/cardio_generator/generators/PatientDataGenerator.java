583d4fa777ad83168fabc56d5c50897a04e9bb85
capybara100 <150798612+capybara100@users.noreply.github.com>
2024-04-21 14:24:30 +0200

    Javadoc comments added to PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,14 @@
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+/**
+* Generates specific patient data
+*/
 public interface PatientDataGenerator {
+    /**
+    * Generates patient data while using the output strategy
+    * @param patientId which is an int
+    * @param ouputStrategy which an OutputStrategy that is being used
+    */
     void generate(int patientId, OutputStrategy outputStrategy);
 }

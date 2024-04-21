afb4211ac538f56a69f8ba8320b1c23c5077e9f1
capybara100 <150798612+capybara100@users.noreply.github.com>
2024-04-21 16:54:04 +0200

    Added Javadoc comments to OutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,14 @@
 package com.cardio_generator.outputs;
-
+/**
+* This interface is charge of how the data is outputed
+*/
 public interface OutputStrategy {
+    /**
+    * Selects how the data is ouputted
+    * @param patientId is a int that identifices the patient
+    * @param timestamp is a long that is the recorded time
+    * @param label is a String that labels the data
+    * @param data is a String that is the data
+    */
     void output(int patientId, long timestamp, String label, String data);
 }

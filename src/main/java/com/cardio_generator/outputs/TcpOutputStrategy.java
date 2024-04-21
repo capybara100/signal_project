721940b89613295a64a270224f03fbbf7d9f54c8
capybara100 <150798612+capybara100@users.noreply.github.com>
2024-04-21 18:14:25 +0200

    Javadoc comments added to TcpOutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -6,12 +6,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 
+/**
+* This class is ouputs health data through a TCP socket
+*/    
 public class TcpOutputStrategy implements OutputStrategy {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
-
+    /**
+    *This constructor creates a TcpOutputStrategy with the port given
+    *@param port which an int that tells which TCP serve will look for client connections
+    */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -31,7 +37,13 @@ public class TcpOutputStrategy implements OutputStrategy {
             e.printStackTrace();
         }
     }
-
+     /**
+    *This output method prints the health data through TCP connection that was selected
+    * @param patientId is a int that identifices the patient
+    * @param timestamp is a long that is the recorded time
+    * @param label is a String that labels the data
+    * @param data is a String that is the given data
+    */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
